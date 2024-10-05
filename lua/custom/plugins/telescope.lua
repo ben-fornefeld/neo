f0e002c560c2c5b5ec2ac081afd05163a9b5ac7f
Ben Fornefeld <ben.fornefeld@gmail.com>
2024-10-05 23:27:57 +0200

    misc

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -85,14 +85,22 @@ return {
       pcall(mod.load_extension, 'fzf')
       pcall(mod.load_extension, 'ui-select')
       pcall(mod.load_extension, 'noice')
+
+
+      local find_opts = { hidden = true, file_ignore_patterns = { ".git/", "node_modules/", "build/", "dist/" } }
+
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = '[F]ind [H]elp' })
       vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = '[F]ind [K]eymaps' })
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [F]iles' })
+      vim.keymap.set('n', '<leader>ff', function()
+        builtin.find_files(find_opts)
+      end, { desc = '[F]ind [F]iles (including hidden, excluding build folders)' })
       vim.keymap.set('n', '<leader>fs', builtin.builtin, { desc = '[F]ind [S]elect Telescope' })
       -- vim.keymap.set('n', '<leader>fg', builtin.grep_string, { desc = '[F]ind current [W]ord' })
-      vim.keymap.set('n', '<leader>fw', builtin.live_grep, { desc = '[F]ind by [G]rep' })
+      vim.keymap.set('n', '<leader>fw', function()
+        builtin.live_grep(find_opts)
+      end, { desc = '[F]ind by [G]rep' })
       vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[F]ind [D]iagnostics' })
       vim.keymap.set('n', '<leader>fr', builtin.resume, { desc = '[F]ind [R]esume' })
       vim.keymap.set('n', '<leader>f.', builtin.oldfiles, { desc = '[F]ind Recent Files ("." for repeat)' })
